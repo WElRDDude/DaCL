8aefbad9ad88f6b249ff00829d43c9c44d95ba18
WElRDDude <125609698+WElRDDude@users.noreply.github.com>
2025-07-11 10:36:26 +0200

    Update manual_trigger.py

--- lab_car_video_system/manual_trigger.py
+++ lab_car_video_system/manual_trigger.py
@@ -1,18 +1,21 @@
 import RPi.GPIO as GPIO
 from queue import Queue
 import time
+import configparser
 
 class ManualTriggerListener:
-    def __init__(self, event_queue):
+    def __init__(self, event_queue, config):
         self.event_queue = event_queue
+        self.config = config
+        self.BUTTON_PIN = int(self.config.get('manual_trigger', 'button_pin'))
+        self.DEBOUNCE_DELAY = float(self.config.get('manual_trigger', 'debounce_delay'))
 
     def start(self):
         GPIO.setmode(GPIO.BCM)
-        BUTTON_PIN = 18
-        GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         while True:
-            if GPIO.input(BUTTON_PIN) == GPIO.LOW:
+            if GPIO.input(self.BUTTON_PIN) == GPIO.LOW:
                 self.event_queue.put("MANUAL_EVENT")
-                time.sleep(0.5)  # Debounce delay
+                time.sleep(self.DEBOUNCE_DELAY)  # Debounce delay
             time.sleep(0.1)  # Small delay to reduce CPU usage
